4899dc8c8c8f0dc1c2f7a77b29d43ca0004a7610
SinanDmrr <sinan-demir@hotmail.com>
2025-04-29 16:43:36 +0200

    Refactoring the Code to React 19 Conventions

--- src/components/IngredientInput/IngredientInput.tsx
+++ src/components/IngredientInput/IngredientInput.tsx
@@ -1,28 +1,40 @@
 import "./IngredientInput.scss";
 import addToList from "../../assets/img/add-to-list.png";
-import {FormEvent, useRef, useState} from "react";
+// Wenn man im Form das Action nutzt dann braucht man kein FormEvent mehr
+// import {FormEvent, useRef, useState} from "react";
+import {useRef, useState} from "react";
 
 function IngredientInput() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [ingredients, setIngredients] = useState<string[]>([]);
 
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault();
-    const targetForm = event.currentTarget as HTMLFormElement;
-    // Dadurch das man das ganze Formevent hier erhält kann man auch die Werte z.B. von Inputfeldern anhand des name-attributes auslesen
-    // Beispiel:
-    // const formData = new FormData(targetForm);
-    // console.log(formData.get("ingredient"));
+  // Veraltet seit React19: Wenn man im Form das Action wie React19 Like nutzt dann braucht man handleSubmit und addIngredient nicht mehr
+  // function handleSubmit(event: FormEvent) {
+  //   event.preventDefault();
+  //   const targetForm = event.currentTarget as HTMLFormElement;
+  //   // Dadurch das man das ganze Formevent hier erhält kann man auch die Werte z.B. von Inputfeldern anhand des name-attributes auslesen
+  //   // Beispiel:
+  //   // const formData = new FormData(targetForm);
+  //   // console.log(formData.get("ingredient"));
 
-    if (inputRef.current) {
-      targetForm.reset();
-    }
-  }
+  //   if (inputRef.current) {
+  //     targetForm.reset();
+  //   }
+  // }
 
-  function addIngredient() {
-    if (inputRef.current && inputRef.current.value != "") {
-      const inputValue = inputRef.current.value.trim();
-      setIngredients([...ingredients, inputValue]);
+  // function addIngredient() {
+  //   if (inputRef.current && inputRef.current.value != "") {
+  //     const inputValue = inputRef.current.value.trim();
+  //     setIngredients([...ingredients, inputValue]);
+  //   }
+  // }
+
+  // Mit React19 (5.Dez.2024) kann man dem action attribut vom Form Element eine Funktion geben was ausgelöst wir beim submit
+  // Dadurch kann man sich addIngredient() sparen und direkt im handleSubmitAction alles ausführen
+  function handleSubmitAction(formData: FormData) {
+    const ingredient = (formData.get("ingredient") as string).trim();
+    if (ingredient != "") {
+      setIngredients([...ingredients, ingredient]);
     }
   }
 
@@ -41,7 +53,8 @@ function IngredientInput() {
 
   return (
     <div id="main">
-      <form id="input-section" onSubmit={handleSubmit} method="POST">
+      {/* <form id="input-section" onSubmit={handleSubmit} method="POST"> */}
+      <form id="input-section" action={handleSubmitAction}>
         <input
           ref={inputRef}
           id="ingredientInput"
@@ -49,7 +62,9 @@ function IngredientInput() {
           placeholder="z.B. Salz, Mehl, Milch"
           name="ingredient"
         />
-        <button onClick={addIngredient}>
+        {/* Mit action im Form kein onclick mehr nötig */}
+        {/* <button onClick={addIngredient}> */}
+        <button type="submit">
           <img src={addToList} alt="Hinzufügen Icon" />
           <p>Hinzufügen</p>
         </button>
