647b6462d29feac020b1bd3e45ce7a6369766943
SinanDmrr <sinan-demir@hotmail.com>
2025-04-30 00:14:00 +0200

    IngredientInput.tsx Component has been splitted, now the Ingridients List will be rendert in the IngredientsList.tsx Component

--- src/components/IngredientInput/IngredientInput.tsx
+++ src/components/IngredientInput/IngredientInput.tsx
@@ -1,59 +1,22 @@
 import "./IngredientInput.scss";
 import addToList from "../../assets/img/add-to-list.png";
-// Wenn man im Form das Action nutzt dann braucht man kein FormEvent mehr
-// import {FormEvent, useRef, useState} from "react";
 import {useRef, useState} from "react";
+import IngredientList from "../IngredientList/IngredientList";
 
 function IngredientInput() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [ingredients, setIngredients] = useState<string[]>([]);
 
-  // Veraltet seit React19: Wenn man im Form das Action wie React19 Like nutzt dann braucht man handleSubmit und addIngredient nicht mehr
-  // function handleSubmit(event: FormEvent) {
-  //   event.preventDefault();
-  //   const targetForm = event.currentTarget as HTMLFormElement;
-  //   // Dadurch das man das ganze Formevent hier erhält kann man auch die Werte z.B. von Inputfeldern anhand des name-attributes auslesen
-  //   // Beispiel:
-  //   // const formData = new FormData(targetForm);
-  //   // console.log(formData.get("ingredient"));
-
-  //   if (inputRef.current) {
-  //     targetForm.reset();
-  //   }
-  // }
-
-  // function addIngredient() {
-  //   if (inputRef.current && inputRef.current.value != "") {
-  //     const inputValue = inputRef.current.value.trim();
-  //     setIngredients([...ingredients, inputValue]);
-  //   }
-  // }
-
   // Mit React19 (5.Dez.2024) kann man dem action attribut vom Form Element eine Funktion geben was ausgelöst wir beim submit
-  // Dadurch kann man sich addIngredient() sparen und direkt im handleSubmitAction alles ausführen
+  // Vor React19 musste man über onSubmit das Event vom Form abfangen und über diesen dann currentTarget nehmen und über diesen an die Name Values ran
   function handleSubmitAction(formData: FormData) {
     const ingredient = (formData.get("ingredient") as string).trim();
     if (ingredient != "") {
       setIngredients([...ingredients, ingredient]);
     }
   }
-
-  function deleteIngredient(indexToDelete: number) {
-    setIngredients(ingredients.filter((_, index) => index !== indexToDelete));
-  }
-
-  const ingredientsList = ingredients.map((ingredient, index) => (
-    <li key={index}>
-      <div id="li-btn-container">
-        <span>{ingredient}</span>
-        <button onClick={() => deleteIngredient(index)}>X</button>
-      </div>
-    </li>
-  ));
-
   return (
     <div id="main">
-      {/* <form id="input-section" onSubmit={handleSubmit} method="POST"> */}
       <form id="input-section" action={handleSubmitAction}>
         <input
           ref={inputRef}
@@ -62,17 +25,15 @@ function IngredientInput() {
           placeholder="z.B. Salz, Mehl, Milch"
           name="ingredient"
         />
-        {/* Mit action im Form kein onclick mehr nötig */}
-        {/* <button onClick={addIngredient}> */}
         <button type="submit">
           <img src={addToList} alt="Hinzufügen Icon" />
           <p>Hinzufügen</p>
         </button>
       </form>
-      <div id="ingredients">
-        <h2>Vorhandene Zutaten:</h2>
-        <ul id="ingredientsList">{ingredientsList}</ul>
-      </div>
+      <IngredientList
+        ingredients={ingredients}
+        setIngredients={setIngredients}
+      />
     </div>
   );
 }
--- src/components/IngredientList/IngredientList.tsx
+++ src/components/IngredientList/IngredientList.tsx
@@ -1,7 +1,33 @@
 import "./IngredientList.scss";
 
-function IngredientList() {
-  return;
+interface IngredientListProps {
+  ingredients: string[];
+  setIngredients: (newIngredients: string[]) => void;
+}
+
+function IngredientList({ingredients, setIngredients}: IngredientListProps) {
+  function deleteIngredient(indexToDelete: number) {
+    // .filter braucht 2 Parameter 1.Element 2.Index da wir das Element also die Zutat an index 0,1,2 usw. nicht brauchen bietet JS und TS
+    // den Platzhalter "_" an was sagt hier muss eigentlich ein Parameter sein aber ich brauche den nicht. Man könnte auch Zutat oder Item oder
+    // so hin geschreiben aber dann wird es Gelb unterkringelt, weil TS meckert das die Variable nie genutzt wird obwohl sie Deklariert wird.
+    setIngredients(ingredients.filter((_, index) => index !== indexToDelete));
+  }
+
+  const ingredientsList = ingredients.map((ingredient, index) => (
+    <li key={index}>
+      <div id="li-btn-container">
+        <span>{ingredient}</span>
+        <button onClick={() => deleteIngredient(index)}>X</button>
+      </div>
+    </li>
+  ));
+
+  return (
+    <div id="ingredients">
+      <h2>Vorhandene Zutaten:</h2>
+      <ul id="ingredientsList">{ingredientsList}</ul>
+    </div>
+  );
 }
 
 export default IngredientList;
